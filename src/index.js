2b8b09f7db86d3b14351a8fc565c7c6a5b4321fe
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-02 14:15:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Challenge 1
+// 1. Modify the function is Windy and return true if the speed is greater than 5 and false if not
+// 2. Call the function and alert ‘It is windy’ if it is windy, Else, alert ‘It is not windy’
+
+function isWindy(speed) {
+return speed >5;
+}
+let speed = prompt ("Do you have a windy day (1-10)? ");
+
+  if (isWindy(speed)) {
+    alert("It is windy");
+  } else {
+    alert("It is not windy");
+  }
+
+
+// Challenge 2
+// 1. Add unit parameter to isWindy
+// 2. if greater than 5 and unit is metric, return true, else return false
+// 3. Test both scenarios
+// isWindy(2, 'imperial') should return false
+// isWindy(20, 'metric') should return true
